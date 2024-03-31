e9b59e7d96c902e9a64305f4edca57ef0e87e27f
valdmntina <duquesakura@gmailcom>
2024-03-30 22:36:33 -0500

    base de datos medio lista

--- ProyectoFinal/Proyecto/views.py
+++ ProyectoFinal/Proyecto/views.py
@@ -24,36 +24,114 @@ def editar(request):
     registro.save()
     return HttpResponse('corregido')
 
-def registro_serpientes(request):
+def registro_serpientes(request): #todas las comentadas ya estan registradas, si las descomento se me jode la papita
+    #INOFENSIVAS
     # serpiente4 = Serpientes.objects.create(nombre_cientifico='Pantherophis guttatus',
     #                                        nombre_comun='Serpiente de maíz',
     #                                        habitat='Campos agricolas y bosques',
     #                                        descripcion='Tamaño pequeño y docil')
     # serpiente4.save()
-    serpiente5 = Serpientes.objects.create(nombre_cientifico='Uromacerinae',
-                                           nombre_comun='Serpiente arborícola',
-                                           habitat='Árboles de los bosques tropicales y subtropicales',
-                                           descripcion='Inofensivas para los humanos')
-    serpiente5.save()
+    # serpiente5 = Serpientes.objects.create(nombre_cientifico='Uromacerinae',
+    #                                        nombre_comun='Serpiente arborícola',
+    #                                        habitat='Árboles de los bosques tropicales y subtropicales',
+    #                                        descripcion='Inofensivas para los humanos')
+    # serpiente5.save()
 
-    serpiente6 = Serpientes.objects.create(nombre_cientifico='Pseudoboa neuwiedii',
-                                           nombre_comun='Serpiente ratonera común',
-                                           habitat='Bosques hasta áreas agrícolas',
-                                           descripcion='Su alimentación se basa en roedores y pequeños mamiferos')
-    serpiente6.save()
+    # serpiente6 = Serpientes.objects.create(nombre_cientifico='Pseudoboa neuwiedii',
+    #                                        nombre_comun='Serpiente ratonera común',
+    #                                        habitat='Bosques hasta áreas agrícolas',
+    #                                        descripcion='Su alimentación se basa en roedores y pequeños mamiferos')
+    # serpiente6.save()
 
-    serpiente7 = Serpientes.objects.create(nombre_cientifico='Drymobius margaritiferus',
-                                           nombre_comun='Serpiente corredora',
-                                           habitat='Bosques',
-                                           descripcion='Rápida velocidad y agilidad para moverse por el suelo y árboles')
-    serpiente7.save()
+    # serpiente7 = Serpientes.objects.create(nombre_cientifico='Drymobius margaritiferus',
+    #                                        nombre_comun='Serpiente corredora',
+    #                                        habitat='Bosques',
+    #                                        descripcion='Rápida velocidad y agilidad para moverse por el suelo y árboles')
+    # serpiente7.save()
 
-    serpiente8 = Serpientes.objects.create(nombre_cientifico='Erythrolamprus bizona',
-                                           nombre_comun='Serpiente falsa coral',
-                                           habitat='Bosques tropicales y sabanas',
-                                           descripcion='Apariencia similar a la serpiente coral, inofensiva para los humanos')
-    serpiente8.save()
-    return HttpResponse('Registradas :D')
+    # serpiente8 = Serpientes.objects.create(nombre_cientifico='Erythrolamprus bizona',
+    #                                        nombre_comun='Serpiente falsa coral',
+    #                                        habitat='Bosques tropicales y sabanas',
+    #                                        descripcion='Apariencia similar a la serpiente coral, inofensiva para los humanos')
+    # serpiente8.save()
+
+    # serpiente9 = Serpientes.objects.create(nombre_cientifico='Drepanoides anomalus',
+    #                                        nombre_comun='Serpiente de liga',
+    #                                        habitat='Áreas boscosas',
+    #                                        descripcion='Inofensiva para los humanos')
+    # serpiente9.save()
+
+    # serpiente10 = Serpientes.objects.create(nombre_cientifico='Chironius monticola',
+    #                                         nombre_comun='Serpiente látigo colombiana',
+    #                                         habitat='Bosques y áreas arbustivas',
+    #                                         descripcion='Conocida por su cuerpo largo y delgado')
+    # serpiente10.save()
+
+    # serpiente11 = Serpientes.objects.create(nombre_cientifico='Pseustes poecilonotus',
+    #                                         nombre_comun='Serpiente maicera',
+    #                                         habitat='Variada',
+    #                                         descripcion='Conocida tambien como serpiente maicera rayada')
+    # serpiente11.save()
+
+    # serpiente12 = Serpientes.objects.create(nombre_cientifico='Helicops angulatus',
+    #                                         nombre_comun='Serpiente de agua colombiana',
+    #                                         habitat='Ríos, lagos y pantanos',
+    #                                         descripcion='Encontrada comúnmente en lugares acuáticos')
+    # serpiente12.save() 
+
+    #VENENOSAS
+
+    # serpiente13 = Serpientes.objects.create(nombre_cientifico='Bothrops asper',
+    #                                         nombre_comun='Terciopelo o Barba amarilla',
+    #                                         habitat='Variada',
+    #                                         descripcion='Coloracion variada con patrones')
+    # serpiente13.save()
+
+    # serpiente14 = Serpientes.objects.create(nombre_cientifico='Bothrops atrox',
+    #                                         nombre_comun='Jergón',
+    #                                         habitat='Selvas y bosques tropicales humedos',
+    #                                         descripcion='Cuerpo grueso y cabeza triangular')
+    # serpiente14.save()
+
+    # serpiente15 = Serpientes.objects.create(nombre_cientifico='Lachesis muta',
+    #                                         nombre_comun='Serpiente de cascabel muda',
+    #                                         habitat='Selvas y bosques tropicales densos',
+    #                                         descripcion='Cuerpo grueso con cabeza grande y triangular')
+    # serpiente15.save()
+
+    # serpiente16 = Serpientes.objects.create(nombre_cientifico='Micrurus spp',
+    #                                         nombre_comun='Serpiente coralillo',
+    #                                         habitat='Selvas, bosques secos, sabanas y zonas semiáridas',
+    #                                         descripcion='Confundidas con serpientes no venenosas')
+    # serpiente16.save()
+    return HttpResponse('Registradassss :DD')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #esto es para una vez ya tenga metidas las serpientes
 # def lista_serpientes(request):
