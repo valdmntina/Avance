0d09f4e91cbf70627454ba6f43f80fb84ea96c8b
Juan Sebastian <juansebas072204@gmail.com>
2024-04-03 21:05:08 -0500

    vincular imagen

--- ProyectoFinal/Proyecto/admin.py
+++ ProyectoFinal/Proyecto/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Serpientes)
+admin.site.register(Caracteristica)
+admin.site.register(Formulario)
--- ProyectoFinal/Proyecto/models.py
+++ ProyectoFinal/Proyecto/models.py
@@ -7,6 +7,10 @@ class Serpientes(models.Model):
     nombre_comun = models.CharField(max_length=100)
     habitat = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=100)
+    foto = models.ImageField(null = True)
+
+    def __str__(self):
+      return self.nombre_comun
 
 class Caracteristica(models.Model):
     venenosa = models.BooleanField(default=False) #especifica el valor por defecto si se intenta ingresar un valor vacio
@@ -14,6 +18,9 @@ class Caracteristica(models.Model):
     alimentacion = models.CharField(max_length=100)
     serpiente = models.OneToOneField(Serpientes, on_delete=models.PROTECT)
    
+    def __str__(self):
+        return self.venenosa
+
 class Formulario(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
